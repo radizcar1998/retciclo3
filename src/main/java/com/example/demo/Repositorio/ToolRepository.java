daa84d2c319622ac947dac7db47d45c86fe67f41
radizcar1998 <113632473+radizcar1998@users.noreply.github.com>
2022-10-17 15:47:59 -0500

    Update ToolRepository.java

--- src/main/java/com/example/demo/Repositorio/ToolRepository.java
+++ src/main/java/com/example/demo/Repositorio/ToolRepository.java
@@ -4,9 +4,9 @@
  */
 package com.example.demo.Repositorio;
 
-import com.example.demo.Interface.ToolInterface;
+import com.example.demo.Interface.CinemaInterface;
 import org.springframework.stereotype.Repository;
-import com.example.demo.Modelo.Tool;
+import com.example.demo.Modelo.Cinema;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,19 +17,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @Repository
 
-public class ToolRepository {
+public class CinemaRepository {
      @Autowired
     private ToolInterface extencionesCrud;
     
-    public List<Tool> getAll(){
-        return (List<Tool>) extencionesCrud.findAll();
+    public List<Cinema> getAll(){
+        return (List<Cinema>) extencionesCrud.findAll();
     }
     
-    public Optional<Tool> getTool(int id){
+    public Optional<Cinema> getCinema(int id){
         return extencionesCrud.findById(id);
     }
     
-    public Tool save(Tool tool){
-        return extencionesCrud.save(tool);
+    public Tool save(Cinema cinema){
+        return extencionesCrud.save(cinema);
     }
 }
